32c493d7f4c8e0b6dfc62fd1a0a6121dc8fc62ab
joelluris <69237151+joelluris@users.noreply.github.com>
2025-11-18 21:38:42 +0400

    Update data.tf

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,39 +1,39 @@
-data "azurerm_client_config" "current" {}
+# data "azurerm_private_dns_zone" "dns_zones" {
+#   for_each = toset([
+#     "privatelink.vaultcore.azure.net",
+#     "privatelink.file.core.windows.net",
+#     "privatelink.queue.core.windows.net",
+#     "privatelink.table.core.windows.net",
+#     "privatelink.uaenorth.azmk8s.io",
+#     "privatelink.azurecr.io",
+#     # "privatelink.postgres.azure.com", # to be enabled in LUNATE-SHARED_SERVICES
+#   ])
+#   provider            = azurerm.LUNATE-SHARED_SERVICES
+#   name                = each.key
+#   resource_group_name = "rg-app-sec-shared-uaen-01"
+# }
 
-data "http" "ip" {
-  url = "https://api.ipify.org/"
-  retry {
-    attempts     = 5
-    max_delay_ms = 1000
-    min_delay_ms = 500
+data "azurerm_bastion_host" "central_bastion" {
+  provider            = azurerm.LUNATE-SHARED_SERVICES
+  name                = "bas-shared-uaen-01"
+  resource_group_name = "rg-app-sec-shared-uaen-01"
+}
+
+locals {
+  dns_zones_map = {
+    "privatelink.vaultcore.azure.net"    = "rg-app-sec-shared-uaen-01"
+    "privatelink.uaenorth.azmk8s.io"     = "rg-app-sec-shared-uaen-01"
+    "privatelink.azurecr.io"             = "rg-app-sec-shared-uaen-01"
+    "privatelink.blob.core.windows.net"  = "rg-net-sec-shared-uaen-01"
+    "privatelink.file.core.windows.net"  = "rg-app-sec-shared-uaen-01"
+    "privatelink.queue.core.windows.net" = "rg-app-sec-shared-uaen-01"
+    "privatelink.table.core.windows.net" = "rg-app-sec-shared-uaen-01"
   }
 }
 
-#checkov:skip=CKV_TF_1:Using Azure Verified Module (AVM) with semantic versioning for maintainability
-# module "regions" {
-#   source  = "Azure/avm-utl-regions/azurerm"
-#   version = "0.5.0"
-# }
-
 data "azurerm_private_dns_zone" "dns_zones" {
-  for_each = toset([
-    "privatelink.vaultcore.azure.net",
-    "privatelink.blob.core.windows.net",
-    # "privatelink.file.core.windows.net",
-    # "privatelink.queue.core.windows.net",
-    # "privatelink.table.core.windows.net",
-    # "privatelink.kubernetes.io",
-    "privatelink.uaenorth.azmk8s.io",
-    "privatelink.azurecr.io",
-    # "privatelink.postgres.azure.com",
-  ])
-  provider            = azurerm.connectivity
+  for_each            = local.dns_zones_map
+  provider            = azurerm.LUNATE-SHARED_SERVICES
   name                = each.key
-  resource_group_name = "rg-hub-dns-uaenorth"
-}
-# Hub VNet for peering
-data "azurerm_virtual_network" "hub" {
-  provider            = azurerm.connectivity
-  name                = var.hub_vnet_name
-  resource_group_name = var.hub_vnet_resource_group_name
+  resource_group_name = each.value
 }
