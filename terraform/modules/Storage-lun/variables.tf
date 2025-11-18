7629c267706ee1c1f3d6d40d1984640cdfc63373
joelluris <69237151+joelluris@users.noreply.github.com>
2025-11-18 21:42:50 +0400

    Update variables.tf

--- terraform/modules/Storage-lun/variables.tf
+++ terraform/modules/Storage-lun/variables.tf
@@ -1,25 +1,18 @@
-variable "location" {
-  type        = string
-  description = "The location/region where the UAMIs will be created"
+variable "storage_accounts" {
+  description = "A map of storage accounts to be created"
+  type = map(object({
+    resource_group_name     = string
+    location                = string
+    storage_account_name    = string
+    account_tier            = string
+    account_replication_type = string
+    https_traffic_only_enabled = bool
+    tags                    = map(string)
+  }))
 }
-
-variable "resource_group_name" {
-  type        = string
-  description = "The name of the resource group"
-}
-
-variable "tags" {
-  type        = map(string)
-  description = "A map of tags to apply to the UAMIs"
-  default     = {}
-}
-
-variable "acr_name" {
-  type        = string
-  description = "The name of the Azure Container Registry"
-}
-
-variable "private_dns_zone_id" {
-  type        = string
-  description = "The ID of the private DNS zone to link with the ACR"
+variable "environment" {}
+variable "snet_details_output" {
+  type = map(object({
+    id = string
+  }))
 }
