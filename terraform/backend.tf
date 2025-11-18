2de609416967638178e15249e685298f6fec5183
joelluris <joelluris@gmail.com>
2025-11-18 23:13:01 +0400

    update

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,5 @@
-subscription_id      = "{SUBSCRIPTION_ID_PRD}"
-resource_group_name  = "{TF_STORAGE_RESOURCE_GROUP}"
-storage_account_name = "{TF_STORAGE_ACCOUNT_NAME}"
-container_name       = "{TF_CONTAINER_NAME}"
-access_key           = "{TF_STORAGE_ACCESS_KEY}"
-use_azuread_auth     = true
+terraform {
+  backend "azurerm" {
+    
+  }
+}
