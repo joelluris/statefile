f48cc0c1b7a0b4dbcd64d73be253cf584a73eafa
joelluris <69237151+joelluris@users.noreply.github.com>
2025-11-18 21:45:40 +0400

    Update variables.tf

--- terraform/modules/IAM/variables.tf
+++ terraform/modules/IAM/variables.tf
@@ -1,25 +1,3 @@
-variable "location" {
-  type        = string
-  description = "The location/region where the UAMIs will be created"
-}
-
-variable "resource_group_name" {
-  type        = string
-  description = "The name of the resource group"
-}
-
-variable "tags" {
-  type        = map(string)
-  description = "A map of tags to apply to the UAMIs"
-  default     = {}
-}
-
-variable "acr_name" {
-  type        = string
-  description = "The name of the Azure Container Registry"
-}
-
-variable "private_dns_zone_id" {
-  type        = string
-  description = "The ID of the private DNS zone to link with the ACR"
+variable "subscription_id" {
+    type = string
 }
