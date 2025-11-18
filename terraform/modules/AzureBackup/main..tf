00cc016f267a13a21944036f2b50de8bbf0d8189
joelluris <69237151+joelluris@users.noreply.github.com>
2025-11-18 21:47:57 +0400

    Update main..tf

--- terraform/modules/AzureBackup/main..tf
+++ terraform/modules/AzureBackup/main..tf
@@ -1,9 +1,48 @@
-resource "azurerm_container_registry" "acr" {
-  name                          = var.acr_name
-  location                      = var.location
-  resource_group_name           = var.resource_group_name
-  sku                           = "Standard"
-  admin_enabled                 = false
-  # public_network_access_enabled = false
-  tags                          = var.tags
+resource "azurerm_recovery_services_vault" "rsv" {
+  for_each            = var.BackupVault
+  name                = each.value.rsv_vault_name
+  location            = each.value.location
+  resource_group_name = each.value.rsv_resource_group_name
+  sku                 = each.value.rsv_vault_sku
+  soft_delete_enabled = each.value.soft_delete_enabled
+  tags                = each.value.tags
+  depends_on = [var.resource_group_output]
+}
+resource "azurerm_backup_policy_vm" "policy" {
+  for_each = var.BackupPolicy
+
+  name                           = each.value.backup_policy_name
+  resource_group_name            = each.value.rsv_resource_group_name
+  recovery_vault_name            = each.value.rsv_vault_name
+  timezone                       = each.value.timezone
+  instant_restore_retention_days = each.value.instant_restore_retention_days
+
+  backup {
+    frequency = each.value.backup_frequency
+    time      = each.value.backup_time
+  }
+
+  retention_daily {
+    count = each.value.retention_daily
+  }
+
+  retention_weekly {
+    count    = each.value.retention_weekly
+    weekdays = each.value.retention_weekly_days
+  }
+
+  retention_monthly {
+    count    = each.value.retention_monthly
+    weeks    = each.value.retention_monthly_week
+    weekdays = each.value.retention_monthly_days
+  }
+
+  retention_yearly {
+    count    = each.value.retention_yearly
+    months   = each.value.retention_yearly_month
+    weeks    = each.value.retention_yearly_week
+    weekdays = each.value.retention_yearly_days
+  }
+
+  depends_on = [azurerm_recovery_services_vault.rsv]
 }
