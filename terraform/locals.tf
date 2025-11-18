0412d1be4f7e9635cbaa72758338e0aaf3223d6a
joelluris <joelluris@gmail.com>
2025-11-18 23:03:48 +0400

    commented the terrascan

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,11 +1,11 @@
-locals {
-  project_code                 = var.project_code
-  environment                  = var.environment
-  location                     = var.location
-  workloads                    = var.workloads
-  resource_name_sequence_start = var.resource_name_sequence_start
+# locals {
+#   project_code                 = var.project_code
+#   environment                  = var.environment
+#   location                     = var.location
+#   workloads                    = var.workloads
+#   resource_name_sequence_start = var.resource_name_sequence_start
 
-  tags = merge(var.tags, {
-    Environment = var.environment
-  })
-}
+#   tags = merge(var.tags, {
+#     Environment = var.environment
+#   })
+# }
