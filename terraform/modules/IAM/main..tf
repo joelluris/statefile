fc48aca6280ee68cfbb3f16f7666686e3aa70d9a
joelluris <69237151+joelluris@users.noreply.github.com>
2025-11-18 21:45:21 +0400

    Update main..tf

--- terraform/modules/IAM/main..tf
+++ terraform/modules/IAM/main..tf
@@ -1,9 +1,31 @@
-resource "azurerm_container_registry" "acr" {
-  name                          = var.acr_name
-  location                      = var.location
-  resource_group_name           = var.resource_group_name
-  sku                           = "Standard"
-  admin_enabled                 = false
-  # public_network_access_enabled = false
-  tags                          = var.tags
+resource "azuread_group" "azure_network_admins" {
+  display_name     = "RoleGroup-Azure-Network-Administrator"
+  security_enabled = true
 }
+
+resource "azuread_group" "azure_security_admins" {
+  display_name     = "RoleGroup-Azure-Security-Administrator"
+  security_enabled = true
+}
+
+output "azure_network_admins_id" {
+  value = azuread_group.azure_network_admins.id
+}
+
+output "azure_security_admins_id" {
+  value = azuread_group.azure_security_admins.id
+}
+
+
+resource "azurerm_role_assignment" "network_admin" {
+  scope                = "/subscriptions/${var.subscription_id}"
+  role_definition_name = "Network Contributor"
+  principal_id         = azuread_group.azure_network_admins.object_id
+}
+
+resource "azurerm_role_assignment" "security_admin" {
+  scope                = "/subscriptions/${var.subscription_id}"
+  role_definition_name = "Security Admin"
+  principal_id         = azuread_group.azure_security_admins.object_id
+}
+
