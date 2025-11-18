830bb0c827d6dc81b44c3cd3dbfe489b1c8ec72a
joelluris <69237151+joelluris@users.noreply.github.com>
2025-11-18 21:44:19 +0400

    Update variables.tf

--- terraform/modules/LogAnalytics/variables.tf
+++ terraform/modules/LogAnalytics/variables.tf
@@ -1,25 +1,13 @@
-variable "location" {
-  type        = string
-  description = "The location/region where the UAMIs will be created"
-}
+variable "location" {}
+variable "tenant_id" {}
+variable "subscription_id" {}
+variable "resource_group_output" {}
 
-variable "resource_group_name" {
-  type        = string
-  description = "The name of the resource group"
-}
-
-variable "tags" {
-  type        = map(string)
-  description = "A map of tags to apply to the UAMIs"
-  default     = {}
-}
-
-variable "acr_name" {
-  type        = string
-  description = "The name of the Azure Container Registry"
-}
-
-variable "private_dns_zone_id" {
-  type        = string
-  description = "The ID of the private DNS zone to link with the ACR"
+variable "loganalytics" {
+  type = map(object({
+    name               = string
+    resource_group_name = string
+    sku                = string
+    tags                         = map(string)
+  }))
 }
