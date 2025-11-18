1488f1712455680eccfad81a40427477c9405c0c
joelluris <69237151+joelluris@users.noreply.github.com>
2025-11-18 21:44:44 +0400

    Update main..tf

--- terraform/modules/KeyVault/main..tf
+++ terraform/modules/KeyVault/main..tf
@@ -1,9 +1,24 @@
-resource "azurerm_container_registry" "acr" {
-  name                          = var.acr_name
-  location                      = var.location
-  resource_group_name           = var.resource_group_name
-  sku                           = "Standard"
-  admin_enabled                 = false
-  # public_network_access_enabled = false
-  tags                          = var.tags
+resource "azurerm_key_vault" "kv" {
+  for_each            = var.key_vault
+  name                = each.value.kv_name
+   tenant_id           = var.tenant_id
+   location            = var.location
+  resource_group_name = var.key_vault[each.key].kv_rg_name
+  sku_name            = each.value.sku
+  tags                = each.value.tags
+  depends_on = [var.resource_group_output]
+
+  network_acls {
+    default_action = "Deny"
+    bypass         = "AzureServices"
+    ip_rules = local.combined_ip_ranges
+  }
+}
+
+locals {
+  combined_ip_ranges = flatten([
+    var.Arcon_PAM_IP,
+    var.umbrella_ip_range,
+    var.AzureDevopsrunner
+  ])
 }
