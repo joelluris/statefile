5c2bb97777a46ac8f290a124daaed96a3892dbd3
joelluris <joelluris@gmail.com>
2025-11-27 06:51:14 +0400

    Add resource_provider_registrations = none to main azurerm provider

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -37,8 +37,9 @@ terraform {
   # }
 }
 provider "azurerm" {
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
+  resource_provider_registrations = "none"
+  tenant_id                       = var.tenant_id
+  subscription_id                 = var.subscription_id
   features {}
 }
 
