e8c0d634160116b6b243094413c4d85f33666243
joelluris <joelluris@gmail.com>
2025-11-27 05:45:14 +0400

    adding iam

--- terraform/modules/IAM/main..tf
+++ terraform/modules/IAM/main..tf
@@ -13,18 +13,6 @@ resource "azuread_group" "azure_vm_contributors" {
   security_enabled = true
 }
 
-output "azure_network_admins_id" {
-  value = azuread_group.azure_network_admins.id
-}
-
-output "azure_security_admins_id" {
-  value = azuread_group.azure_security_admins.id
-}
-
-output "azure_vm_contributors_id" {
-  value = azuread_group.azure_vm_contributors.id
-}
-
 resource "azurerm_role_assignment" "network_admin" {
   scope                = "/subscriptions/${var.subscription_id}"
   role_definition_name = "Network Contributor"
