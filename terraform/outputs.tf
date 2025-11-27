e26f09185010a3e5c1f552aa632cbc45b315f90b
joelluris <joelluris@gmail.com>
2025-11-27 06:46:00 +0400

    remove output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,13 +1,13 @@
-# PostgreSQL Private DNS Zone
-output "postgresql_private_dns_zone_id" {
-  description = "PostgreSQL Private DNS Zone ID"
-  value       = data.azurerm_private_dns_zone.dns_zones["privatelink.postgres.database.azure.com"].id
-}
+# # PostgreSQL Private DNS Zone
+# output "postgresql_private_dns_zone_id" {
+#   description = "PostgreSQL Private DNS Zone ID"
+#   value       = data.azurerm_private_dns_zone.dns_zones["privatelink.postgres.database.azure.com"].id
+# }
 
-output "snet_details_output" {
-  value = module.VirtualNetwork.snet_details_output
-}
+# output "snet_details_output" {
+#   value = module.VirtualNetwork.snet_details_output
+# }
 
-output "vnet_details_output" {
-  value = module.VirtualNetwork.vnet_details_output
-}
+# output "vnet_details_output" {
+#   value = module.VirtualNetwork.vnet_details_output
+# }
